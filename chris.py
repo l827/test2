e2eaa1994b2da5866ed725d154eaf4f2ccdbe2ea
Liz <>
2020-03-30 21:35:05 +0100

    First commit of dog code by Chris.

---
+++ chris.py
@@ -0,0 +1,16 @@
+from itertools import *
+
+print("Hello world!")
+
+for i, num in enumerate(range(54,65)):
+	print("Number {0} is {1}".format(i,num))
+
+stupid=0
+while(True):
+	dogName = input("Who is the best dog?")
+	if dogName=="Laika":
+		print("That's right! Oogibooboo!")
+		break
+	else:
+		stupid += 1
+		print("Errr hello??! You've got it wrong {0} time{1}\nTry again...".format(stupid, "" if stupid==1 else "s, dummy"))
